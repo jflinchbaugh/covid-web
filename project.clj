79aaa2286fc9abc98ca94677dfef4b50b4a9d858
John Flinchbaugh <john@hjsoft.com>
2020-12-01 00:37:56 -0500

    add cljs-http lib

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
 
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.773"]
-                 [reagent "1.0.0-rc1"]]
+                 [reagent "1.0.0-rc1"]
+                 [cljs-http "0.1.46"]]
 
   :plugins [[lein-cljsbuild "1.1.7"]
             [lein-figwheel "0.5.20"]]

f1fb7d218187a26536136c41fc1622c74cefcf7c
John Flinchbaugh <john@hjsoft.com>
2020-12-21 13:48:19 -0500

    upgrade to reagent 1.0.0

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
 
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.773"]
-                 [reagent "1.0.0-rc1"]
+                 [reagent "1.0.0"]
                  [cljs-http "0.1.46"]]
 
   :plugins [[lein-cljsbuild "1.1.7"]
